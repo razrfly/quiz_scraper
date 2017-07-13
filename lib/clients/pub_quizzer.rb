38bca9c79de77833a9c89e8f377a5d8d492710f2
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-13 13:51:03 +0200

    Set up rules for extracting table data with nokogiri
    -> A strongly experimental approach. Static reference to constant
       will be initialized only once. In that way we can use memoization
       to cache 'headers' and 'pagination'. It is quite efficient also.
    
    -> headers have been memoized for future use case
    
    -> pagination links have been memoized to allow for fetching
       all data at once.
    
    -> table rows, and table haven't been memoized as they are changeable

--- lib/clients/pub_quizzer.rb
+++ lib/clients/pub_quizzer.rb
@@ -1,6 +1,32 @@
 class PubQuizzer
   Table = ->(response) {
-    # Here will be (already is) nokogirii parsing fun
+    table = process(response) { |document| document.css('#rounded-corner') }
+
+    @headers ||= -> {
+      headers = table.css('thead').css('tr').css('th').text and headers[0] = ''
+      headers = headers.split("\n").map do |header|
+        parameterize(header.strip, separator: ?_)
+      end
+      headers << 'reference'
+    }.call
+
+    @paginate_links ||= -> {
+      links = table.css('tfoot').css('tr').css('td.rounded-foot-left').css('b')
+      links.css('a[href]').each_with_object({}).with_index do |(link, result), i|
+        result[i + 1] = link["href"].sub(%r(#{base_url}), '')
+      end
+    }.call
+
+    trows = -> {
+      trows = table.css('tbody').css('tr')
+      trows.each_with_object([]) do |row, result|
+        reference, data = row.css('a[href]').first['href'], row.text.split(?\n)
+        data[2].sub!(/^.-./, '') # location fix
+        result << (data[1..-1] << reference)
+      end
+    }.call
+
+    { headers: @headers, trows: trows, paginate_links: @paginate_links }
   }
   private_constant(:Table)
 
