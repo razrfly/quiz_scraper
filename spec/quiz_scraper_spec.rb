a47eef3147ac2ecac6c84fa548e0b1b372a702b5
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-14 20:58:32 +0200

    Scraper initialization tests

--- spec/quiz_scraper_spec.rb
+++ spec/quiz_scraper_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe QuizScraper do
   it "has a version number" do
     expect(QuizScraper::VERSION).not_to be nil
   end
+
+  context "on initialize" do
+    let(:scraper_adapter) { QuizScraper::Scraper }
+
+    it "raises ArgumentError when initialize without argument" do
+      expect { scraper_adapter.new }.to raise_error(ArgumentError)
+    end
+
+    it "raises UndefinedScraperError when initialize with incorrect argument" do
+      error = QuizScraper::ScraperLoader::UndefinedScraperError
+      expect { scraper_adapter.new(:missing) }.to raise_error(error)
+    end
+
+    it "sets up scraper accessor for existing scraper" do
+      adapter = scraper_adapter.new(:pub_quizzer)
+      expect(adapter.scraper).to eq(QuizScraper::PubQuizzer)
+    end
+  end
 end
