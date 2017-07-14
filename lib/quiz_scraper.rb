adf3de62a4f312dfa153f44de54a085ebaf6e9df
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-14 16:05:25 +0200

    Move specific scrapers under global gem namespace

--- lib/quiz_scraper.rb
+++ lib/quiz_scraper.rb
@@ -3,7 +3,7 @@ require "quiz_scraper/scraper_loader"
 require "quiz_scraper/scraper"
 require "quiz_scraper/request_handler"
 require "quiz_scraper/request_processor"
-require "clients/pub_quizzer"
+require "quiz_scraper/scrapers/pub_quizzer"
 
 module QuizScraper
 end
