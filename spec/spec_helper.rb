c056406ee84a8a35f43fe75560119747c18343b8
Aleksandra Karwowska <kjerte@gmail.com>
2017-07-17 16:15:05 +0200

    add webmock gem and configure it in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require "bundler/setup"
 require "quiz_scraper"
+require "webmock/rspec"
+WebMock.disable_net_connect!(:allow_localhost => true)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
