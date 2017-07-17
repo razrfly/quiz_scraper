486019fcb67c7ebf68470190e0a776ef6c46751f
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-17 16:42:30 +0200

    Set up functional object to fetch credentials from base-url page response

--- lib/quiz_scraper/scrapers/geeks_who_drink.rb
+++ lib/quiz_scraper/scrapers/geeks_who_drink.rb
@@ -1,5 +1,18 @@
 module QuizScraper
   module GeeksWhoDrink
+    Credentials = ->(response) {
+      scripts = process(response) { |document| document.css('script') }
+      script = scripts.children.first
+
+      data = script.text =~ /\{.*\}/ and (data = $& || '{}')
+
+      parsed_data = JSON.parse(data).dig("settings", "elasticsearch", "host")
+      parsed_data =~ /^https:\/\/(.*):(.*)@(.*)\z/
+
+      a = { user: $1, password: $2, host: "https://#{$3}" }
+    }
+    private_constant(:Credentials)
+
     class << self
       attr_accessor :base_url, :paginated
 
