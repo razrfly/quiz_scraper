6090c15624b1bece01107f43eb2937c63e0c8331
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-19 16:48:58 +0200

    Set up request params directly instead of using credentials provided on site

--- lib/quiz_scraper/scrapers/geeks_who_drink.rb
+++ lib/quiz_scraper/scrapers/geeks_who_drink.rb
@@ -21,18 +21,47 @@ module QuizScraper
       GeeksWhoDrink.paginated = false
 
       def find_all
-        credentials = Credentials.(send_request('/schedule'))
-
         params = {
-          host: credentials[:host],
-          basic_auth: {
-            user: credentials[:user],
-            password: credentials[:password]
-          }
+          host: "https://dori-us-east-1.searchly.com",
+          headers: {
+            "Host" => "dori-us-east-1.searchly.com",
+            "User-Agent" => "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'",
+            "Accept" => "application/json",
+            "Accept-Language" => "en-US,en;q=0.5",
+            "Authorization" => "Basic d2Vic2l0ZTpxaGJnbDVsczJ1Zm12cjI1aHFwNGhtdGVyaWZhbnplNw==",
+            "Content-Type" => "application/x-www-form-urlencoded; charset=UTF-8",
+            "Referer" => "https://www.geekswhodrink.com/schedule",
+            "origin" => "https://www.geekswhodrink.com",
+            "DNT" => "1",
+            "Connection" => "keep-alive"
+          },
+          body: {
+            :query => {
+              :bool => {
+                :must => [
+                  { :match => { :brands => "gwd" } }
+                ],
+                :must_not => [
+                  { :match => { :status => "Terminated" } },
+                  { :match => { :status => "Content Only" } },
+                  { :match => { :status => "On Hiatus" } },
+                  { :match => { :status => "Failed" } }
+                ],
+                :should => [
+                  { :match => { :status => "Active" } },
+                  { :match => { :status => "In Progress" } },
+                  { :match => { :status => "Ready to Boot" } }
+                ]
+              }
+            },
+            :sort => [
+              { :"venueName.venueName_na" => {} }
+            ]
+          }.to_json
         }
 
         collection = Collection.(
-          send_request('/website/venue/_search?from=0&size=2000', params)
+          send_request('/website/venue/_search?from=0&size=2000', :post, params)
         )
 
         collection.map { |item| QuizScraper::Quiz.new(item, source: self) }
