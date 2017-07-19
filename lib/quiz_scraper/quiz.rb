c31b5988e655577778d30971b498b094f81ce5cd
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-19 11:08:41 +0200

    Expand QuizScraper::Quiz object wit scrape-status
    -> scrape-status will be helpful for further use
    -> delegated to source(scraper) module where it is properly configured

--- lib/quiz_scraper/quiz.rb
+++ lib/quiz_scraper/quiz.rb
@@ -1,13 +1,20 @@
 module QuizScraper
   class Quiz
-    attr_reader :source, :name, :reference, :raw_data
+    attr_reader :source, :name, :reference, :raw_data, :scrape_status
 
-    def initialize(params = {}, source:)
+    def initialize(params = {}, source:, origin:)
       @source = source
+      @scrape_status = resolve_status(origin)
 
       params.each do |key, value|
         instance_variable_set("@#{key}", value)
       end
+    end
+
+    private
+
+    def resolve_status(origin)
+      source.scrape_status[origin]
     end
   end
 end
