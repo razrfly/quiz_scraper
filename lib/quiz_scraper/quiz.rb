d6c583e705232405ef010e30417e545f66b03068
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-20 16:40:13 +0200

    Remove origin and source accessors from QuizScraper::Quiz
    -> They could be useful later, but for now we do not need them
    -> Scrape status will be provided directly from initialize params

--- lib/quiz_scraper/quiz.rb
+++ lib/quiz_scraper/quiz.rb
@@ -1,20 +1,13 @@
 module QuizScraper
   class Quiz
-    attr_reader :source, :name, :reference, :raw_data, :scrape_status
+    attr_reader :name, :reference, :raw_data, :scrape_status
 
-    def initialize(params = {}, source:, origin:)
-      @source = source
-      @scrape_status = resolve_status(origin)
+    def initialize(params = {})
+      @scrape_status = params.delete(:scrape_status)
 
       params.each do |key, value|
         instance_variable_set("@#{key}", value)
       end
-    end
-
-    private
-
-    def resolve_status(origin)
-      source.scrape_status[origin]
     end
   end
 end
