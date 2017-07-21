f45d0c1cdfb90d42aceb26a66f7e7eff7cf85e4d
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-21 15:54:52 +0200

    Add test for page param extraction method

--- spec/quiz_scraper/scraper_spec.rb
+++ spec/quiz_scraper/scraper_spec.rb
@@ -40,5 +40,60 @@ RSpec.describe QuizScraper::Scraper do
     it "has +find+ method defined for external usage" do
       expect(adapter.public_instance_methods).to include(:find)
     end
+
+    context "page parameter extraction" do
+      let(:scraper) { adapter.new(:pub_quizzer) }
+
+      # We don't want to use full scale of +find_all+ method behavior,
+      # so I mimic that behavior by defining singleton method.
+      # This is just kind of container to test +extract_page_params+
+      # method.
+
+      let(:extend_scraper) {
+        def scraper.find_all(params = {})
+          extract_page_param(params)
+        end
+      }
+
+      it "returns 'default' value for empty params" do
+        extend_scraper
+        expect(scraper.find_all).to eq(:default)
+      end
+
+      it "returns 'default' value for unpermitted params keys" do
+        extend_scraper
+        expect(scraper.find_all(not_allowed: 'not allowed')).to eq(:default)
+      end
+
+      it "returns 'default' value for correct key and nil value" do
+        extend_scraper
+        expect(scraper.find_all(page: nil)).to eq(:default)
+      end
+
+      it "returns 'default' value for correct key and unpermitted value" do
+        extend_scraper
+        expect(scraper.find_all(page: [])).to eq(:default)
+      end
+
+      it "returns 'default' value for correct key and unpermitted String value" do
+        extend_scraper
+        expect(scraper.find_all(page: 'nope')).to eq(:default)
+      end
+
+      it "returns value for correct key and permitted String value" do
+        extend_scraper
+        expect(scraper.find_all(page: '1')).to eq(1)
+      end
+
+      it "returns value for correct key and permitted Integer value" do
+        extend_scraper
+        expect(scraper.find_all(page: 1)).to eq(1)
+      end
+
+      it "returns 'all' value when invoked with custom :all page param" do
+        extend_scraper
+        expect(scraper.find_all(page: :all)).to eq(:all)
+      end
+    end
   end
 end
