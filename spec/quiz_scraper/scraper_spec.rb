b3eab74edf973c7cf0c5ee146f4754f6adba0765
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-21 16:14:15 +0200

    Move version test from specific module to global namespace module tests

--- spec/quiz_scraper/scraper_spec.rb
+++ spec/quiz_scraper/scraper_spec.rb
@@ -1,10 +1,6 @@
 require "spec_helper"
 
 RSpec.describe QuizScraper::Scraper do
-  it "has a version number" do
-    expect(QuizScraper::VERSION).not_to be nil
-  end
-
   let(:namespace) { QuizScraper }
   let(:adapter) { QuizScraper::Scraper }
 
---
+++ spec/quiz_scraper_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+RSpec.describe QuizScraper do
+  it "has a version number" do
+    expect(QuizScraper::VERSION).not_to be nil
+  end
+end
