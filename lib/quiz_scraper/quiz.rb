40e09da79f404405bb8cc4fdc5d73bd79b52fc5b
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-24 11:39:25 +0200

    Add scrape-status predicates to QuizScraper::Quiz public interface
    -> I would ask object about it's fully/partially state
       rather than using primitives classes.

--- lib/quiz_scraper/quiz.rb
+++ lib/quiz_scraper/quiz.rb
@@ -5,11 +5,19 @@ module QuizScraper
     def initialize(params = {})
       @scrape_status = params.delete(:scrape_status)
 
-      build_quiz_location(params.delete(:location)) if quiz_full?
+      build_quiz_location(params.delete(:location)) if scrape_full?
 
       params.each do |key, value|
         instance_variable_set("@#{key}", value)
       end
+    end
+
+    def scrape_full?
+      scrape_status == :full
+    end
+
+    def scrape_partial?
+      scrape_status == :partial
     end
 
     private
