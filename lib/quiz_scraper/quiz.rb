5e50a28dc87d82e4205ac629bc57d1fc84168429
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-07-24 11:34:33 +0200

    Associate quiz location builder with quiz for fully scraped quizes

--- lib/quiz_scraper/quiz.rb
+++ lib/quiz_scraper/quiz.rb
@@ -1,13 +1,21 @@
 module QuizScraper
   class Quiz
-    attr_reader :name, :reference, :raw_data, :scrape_status
+    attr_reader :name, :scrape_status, :reference, :location, :raw_data
 
     def initialize(params = {})
       @scrape_status = params.delete(:scrape_status)
 
+      build_quiz_location(params.delete(:location)) if quiz_full?
+
       params.each do |key, value|
         instance_variable_set("@#{key}", value)
       end
+    end
+
+    private
+
+    def build_quiz_location(params)
+      @location = QuizLocation.new(params)
     end
   end
 end
