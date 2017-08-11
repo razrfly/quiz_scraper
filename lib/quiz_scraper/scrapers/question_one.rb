8bfd48720df3eea3271225609bc4fd7ea2bcd7db
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-08-11 10:16:59 +0200

    Setup config accessors for QuestionOne scraper

--- lib/quiz_scraper/scrapers/question_one.rb
+++ lib/quiz_scraper/scrapers/question_one.rb
@@ -1,6 +1,14 @@
 module QuizScraper
   module QuestionOne
     class << self
+      attr_accessor :base_url, :paginated, :scrape_status
+
+      QuestionOne.base_url = 'http://questionone.com'
+      QuestionOne.paginated = false
+      QuestionOne.scrape_status = {
+        :find_all => :full
+      }
+
       def find_all
       end
     end
